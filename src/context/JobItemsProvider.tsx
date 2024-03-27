c6738e7d2495811fe89f1e46072346abe64db799
bhuvan2205 <bhuvancs2205@gmail.com>
2024-03-27 09:26:45 +0530

    Optimize: JobItems Provider using memo

--- src/context/JobItemsProvider.tsx
+++ src/context/JobItemsProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useMemo, useState } from "react";
+import { createContext, ReactNode, useCallback, useMemo, useState } from "react";
 import { useJobItems, useSearchTextContext } from "../lib/hooks";
 import { JobItem, PageDirectionProps, SortMethods } from "../lib/type";
 import { REVIEWS_PER_PAGE } from "../constants/jobItems";
@@ -6,7 +6,6 @@ import { REVIEWS_PER_PAGE } from "../constants/jobItems";
 type ContextProps = {
     isLoading: boolean;
     sortedAndSlicedJobItems: JobItem[],
-    jobItems: JobItem[],
     handleChangePage: (direction: PageDirectionProps) => void,
     handleChangeSort: (newSortBy: SortMethods) => void,
     totalJobItems: number,
@@ -34,10 +33,11 @@ const JobItemsProvider = ({ children }: JobItemsProviderProps) => {
     const endIndex = startIndex + REVIEWS_PER_PAGE;
 
     const jobItemsSliced = useMemo(() => {
+        const jobItems = [...(data?.jobItems || [])];
         if (sortBy === 'recent') {
-            return data?.jobItems?.sort((a, b) => a.daysAgo - b.daysAgo) ?? [];
+            return jobItems?.sort((a, b) => a.daysAgo - b.daysAgo);
         }
-        return data?.jobItems?.sort((a, b) => b.relevanceScore - a.relevanceScore) ?? [];
+        return jobItems?.sort((a, b) => b.relevanceScore - a.relevanceScore);
     }, [data?.jobItems, sortBy]);
 
     const sortedAndSlicedJobItems = useMemo(() => {
@@ -47,32 +47,24 @@ const JobItemsProvider = ({ children }: JobItemsProviderProps) => {
         return jobItemsSliced.slice(startIndex, endIndex);
     }, [endIndex, jobItemsSliced, sortBy, startIndex]);
 
-    const handleChangePage = (direction: PageDirectionProps) => {
+    const handleChangePage = useCallback((direction: PageDirectionProps) => {
 
         if (direction === 'next') {
-            setCurrentPage((prev) => {
-                if (prev === totalPageCount) {
-                    return prev;
-                }
-                return prev + 1;
-            });
+            setCurrentPage((prev) => prev + 1);
         } else if (direction === 'prev') {
-            setCurrentPage((prev) => {
-                if (prev === 1) {
-                    return 1;
-                }
-                return prev - 1;
-            });
+            setCurrentPage((prev) => prev - 1);
         }
-    };
+    }, []);
 
     const handleChangeSort = (newSortBy: SortMethods) => {
         setCurrentPage(1);
         setSortBy(newSortBy);
     };
 
+    const contextValue = useMemo(() => ({ isLoading, sortedAndSlicedJobItems, handleChangePage, handleChangeSort, totalJobItems, totalPageCount, sortBy, currentPage }), [currentPage, handleChangePage, isLoading, sortBy, sortedAndSlicedJobItems, totalJobItems, totalPageCount]);
+
     return (
-        <JobItemsContext.Provider value={{ jobItems: data?.jobItems ?? [], isLoading, sortedAndSlicedJobItems, handleChangePage, handleChangeSort, totalJobItems, totalPageCount, sortBy, currentPage }}>{children}</JobItemsContext.Provider>
+        <JobItemsContext.Provider value={contextValue}>{children}</JobItemsContext.Provider>
     );
 };
 
